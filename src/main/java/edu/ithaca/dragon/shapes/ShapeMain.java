7e9daa408fe514cde4da32a7491294c715eb70e9
bridgetsburns <bburns@ithaca.edu>
2024-09-08 15:26:44 -0400

    list of shapes

--- src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
+++ src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
@@ -3,6 +3,8 @@ package edu.ithaca.dragon.shapes;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Random;
+
 
 public class ShapeMain {
     
@@ -37,5 +39,23 @@ public class ShapeMain {
         double longestLine = rectangleList.get(j).longestLineWithin();
         System.out.println("Rectangle " + (j+1) + ": area = " + area + ", longest line = " + longestLine);
     }
+
+    List<ShapeInterface> shapeList = new ArrayList<ShapeInterface>(); 
+    for (int i = 0; i < 10; i++){
+        Random random = new Random();
+        int randint = random.nextInt(2);
+        if (randint == 0){
+            shapeList.add(new Rectangle(10,20));
+        }
+        if (randint == 1){
+            shapeList.add(new Triangle(8, 18));
+        }
+        if (randint == 2){
+            shapeList.add(new Circle(15));
+        }
+    }
+
+
+
     }
 }
